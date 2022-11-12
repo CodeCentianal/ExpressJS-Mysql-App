53696c40c369aa6878c51e1972495231f2dd3d1a
Sri Ram <63453826+CodeCentianal@users.noreply.github.com>
2022-11-12 19:31:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config(); 
+const express = require('express');
+const app = express();
+const port = process.env.port;
+const mysql = require("mysql");
+const morgan = require('morgan');
+
+app.use(express.json());
+app.use(morgan(':method :url :status :res[content-length] :response-time :date[web]'))
+
+
+app.get('/', (req, res)=>{
+  res.send('Hello wolrd');
+})
+
+app.listen(port,(err)=>{
+    if(err) throw err;
+    console.log("listening to the port!")
+})
+
